c473608f4d1ddbe83428abca7350212d184b34d3
Simon Plakolb <s.plakolb@gmail.com>
2020-12-17 13:35:34 +0100

    Add environment class to core module

---
+++ predatorpreypy/core.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class Environment:
+    def __init__(self, dims, n_predators, n_prey):
+        x_max, y_max = dims
+        self.X_MAX = x_max
+        self.Y_MAX = y_max
+        
+        self.predators = [
+            Predator(
+                self,
+                np.random.randint(0, x_max),
+                np.random.randint(0, y_max),
+                np.random.randint(FOOD_MIN_INIT, FOOD_MAX_INIT)
+                ) 
+            for _ in range(n_predators)
+            ]  
+        
+        self.prey = [
+            Prey(
+                self,
+                np.random.randint(0, x_max),
+                np.random.randint(0, y_max),
+                np.random.randint(FOOD_MIN_INIT, FOOD_MAX_INIT)
+                ) 
+            for _ in range(n_prey)
+            ]
+    
+    def tick(self):
+        # breed
+        for p in self.prey + self.predators:
+            p.breed()
+            
+        # move
+        for p in self.prey + self.predators:
+            dx = np.random.randint(0,3)-1
+            dy = np.random.randint(0,3)-1
+            
+            p.move(dx, dy)
+        
+        # feed
+        for p in self.prey:
+            p.eat(np.random.randint(0,3))
+            
+        for p in self.predators:
+            p.eat()
+            
+        # check alive
+        for p in self.prey:
+            if p.energy <= 0:
+                self.prey.remove(p)
+        
+        for p in self.predators:
+            if p.energy <= 0:
+                self.predators.remove(p)
+    
